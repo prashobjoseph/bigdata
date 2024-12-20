f2505b378e6c2da319543aa838ba278e18fc3e62
prashobjoseph <prashobjoseph@gmail.com>
2024-12-20 15:05:03 +0000

    updated

--- Pyspark/full_load_postgresToHive.py
+++ Pyspark/full_load_postgresToHive.py
@@ -3,10 +3,10 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local").appName("PrashobProject").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "person").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "prashtest").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
-df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.personprashob")
+df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.prashtest")
 print("Successfully Load to Hive")
 
 # spark-submit --master local[*] --jars /var/lib/jenkins/workspace/nagaranipysparkdryrun/lib/postgresql-42.5.3.jar src/full_load_postgresToHive.py
